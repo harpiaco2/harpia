f806b168126ae184e97f9bae229d0557907de035
vlruiz108 <vlruiz@hotmail.com>
2024-08-20 19:22:12 -0300

    chore: ajustes de pacote

--- src/app/api/data/images.ts
+++ src/app/api/data/images.ts
@@ -3,14 +3,15 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export async function uploadImage(file: File, formId: string | undefined): Promise<string | null> {
     const storageRef = ref(storage, `images/${formId}/${file.name}`);
-    
+
     try {
         const snapshot = await uploadBytes(storageRef, file);
+        console.log('Snapshot:', snapshot);
         const downloadURL = await getDownloadURL(snapshot.ref);
         console.log('Image uploaded successfully:', downloadURL);
         return downloadURL;
     } catch (error) {
         console.error('Error uploading image:', error);
-        return null; // Retorna null ou uma string de erro dependendo do caso
+        return null;
     }
 }
