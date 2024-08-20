1f210e06c493513a3451364f416df04afed80962
vlruiz108 <vlruiz@hotmail.com>
2024-08-20 20:34:47 -0300

    chore: ajustando para configuracao inicial

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Habilita exportação estática
-    trailingSlash: true, // Adiciona barra no final dos caminhos
-  };
+const nextConfig = {};
   
   export default nextConfig;  
   
